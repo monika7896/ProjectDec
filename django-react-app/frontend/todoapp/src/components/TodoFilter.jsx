b944bd3d3b0ccdeda47e4df2a55b683c5d2d6498
monicas-kumari <monika.kumari@leptonmaps.com>
2023-12-31 08:05:30 +0530

    multiple commit

---
+++ django-react-app/frontend/todoapp/src/components/TodoFilter.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TodoFilter = () => {
+    return (
+        <select name="" id="">
+            <option value="Active"> Active</option>
+            <option value="Completed">Completed</option>
+        </select>
+    )
+}
+
+export default TodoFilter
